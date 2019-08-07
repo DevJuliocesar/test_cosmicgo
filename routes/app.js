126820c65130a6300055a601e6da187b86a2a369
julio cesar maldonado moreno <jcmaldonadom@ufpso.edu.co>
2019-08-07 10:59:30 -0500

    asignando frontend a pantalla inicial

--- routes/app.js
+++ routes/app.js
@@ -1,27 +1,7 @@
 var express = require('express');
 
-const { Pool } = require('pg');
-const { DATABASE_URL } = process.env;
-const pool = new Pool({
-  connectionString: DATABASE_URL,
-  ssl: true
-});
-
 var app = express();
 
 app.get('/', (req, res, next) => res.render('pages/index'));
-
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM users');
-    const results = { results: result ? result.rows : null };
-    res.render('pages/db', results);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send('Error ' + err);
-  }
-});
 
 module.exports = app;
