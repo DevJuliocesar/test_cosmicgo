bd20da0d4d77e2db095a9864d4eeb58815199b10
julio cesar maldonado moreno <jcmaldonadom@ufpso.edu.co>
2019-08-06 21:51:50 -0500

    mostrando users db

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const PORT = process.env.PORT || 5000;
+
 const app = express();
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 // CORS
 app.use(function(req, res, next) {
@@ -13,6 +21,12 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
+
+/* assets */
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs');
 
 /* parse application/x-www-form-urlencoded */
 app.use(
@@ -31,5 +45,5 @@ let appRoutes = require('./routes/app');
 app.use('/', appRoutes);
 
 app.listen(PORT, () => {
-  console.log('Express server puerto 5000 online');
+  console.log(`Express server puerto ${PORT} online`);
 });
--- routes/app.js
+++ routes/app.js
@@ -1,12 +1,26 @@
 var express = require('express');
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 var app = express();
 
-app.get('/', (req, res, next) => {
-  res.status(200).json({
-    ok: true,
-    mensaje: 'Petición realizada correctamente'
-  });
+app.get('/', (req, res, next) => res.render('pages/index'));
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM users');
+    const results = { results: result ? result.rows : null };
+    res.render('pages/db', results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send('Error ' + err);
+  }
 });
 
 module.exports = app;
